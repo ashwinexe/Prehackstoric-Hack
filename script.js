d94a14db044c785db1820e72186672a3d01bf4e8
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2021-09-11 00:34:38 +0530

    skeleton structure

---
+++ script.js
@@ -0,0 +1,8 @@
+var character = document.getElementById("character");
+var obstacle = document.getElementById("obstacle");
+var score = 0;
+function jump(){
+    if(character.classList.contains("jump")){
+        return;
+    }
+}
