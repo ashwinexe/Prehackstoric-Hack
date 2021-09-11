4085eb126240a2f3b0dd55afa69c1176db50d76d
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2021-09-11 10:44:35 +0530

    ❌high score displayes but loses on refresh

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var character = document.getElementById("character");
 var obstacle = document.getElementById("obstacle");
 var score = 0;
+var highScore = 0
+var stopScore = false;
 var gameOverScreen = document.getElementById("gameOverScreen");
 
 // function moveObstacle(){
 function keyPressed(event){
     var key = event.keyCode;
-    console.log(key);
 
     //jumps if key is spacebar, upArrow or 'w'
     if(key == 32 || key == 38 || key == 87){
@@ -28,18 +29,21 @@ var checkDead = setInterval(() => {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
     //if character and obstacle touches
+    // alert("Game Over");
     if(characterTop >= 130 && obstacleLeft <= 20 && obstacleLeft >= -20)
     {
-        // alert("Game Over");
+        stopScore = true;
         obstacle.style.animation = "none";
         gameOverScreen.style.display = "block";
         document.getElementById("scoreSpan2").innerHTML = Math.floor(score/100);
         //set new highScore 
         if(score > highScore) highScore = score;
         score = 0;
+        console.log(highScore);
+        document.getElementById("highScore").innerHTML = Math.floor(highScore/100);
             
     }
-    else 
+    else if(stopScore == false) 
     {
             score++;
             document.getElementById("scoreSpan").innerHTML = Math.floor(score/100);
@@ -47,3 +51,7 @@ var checkDead = setInterval(() => {
 
 
     }, 10);
+
+    function restart(){
+        document.location.reload();
+    }
